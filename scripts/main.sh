7a808498afad32cbb7bd90942ea8c31e0e7bf068
Peyman Mohtashami <p.mohtashami@gmail.com>
2024-07-03 18:31:05 +0200

    Add UPDATE subject

--- scripts/main.sh
+++ scripts/main.sh
@@ -42,10 +42,29 @@ echo -e "Login\tExternalId\tProjectName"
 echo "$lookup_table" | column -t -s $'\t'
 echo
 
+selected_subject=$(echo "$subjects_response" | jq '[.[] | select(.externalId == "peyman-001")][0]')
+
+updated_subject=$(echo "$selected_subject" | jq '
+                  . + {"project": {"id": 4452, "projectName": "MYSKIN"}}')
+updated_subject=$(echo "$updated_subject" | jq '
+                  (.attributes) |= {"age": 53, "gender": "male"}')
+
+
+echo "$updated_subject"
+
+# Update a Subject
+updated_subject_response=$(curl --silent --location --request PUT "${baseUrl}/managementportal/api/subjects" \
+--header 'Content-Type: application/json' \
+--header "Authorization: Bearer $accessToken" \
+--data "$updated_subject")
+
+echo "UPDATED SUBJECTS"
+echo "$updated_subject_response"
+echo
 
 # Get Topics
 topics_response=$(curl --silent --location "${baseUrl}/kafka/topics" \
  --header "Authorization: Bearer $accessToken")
-echo "TOPICS" 
+echo "TOPICS"
 echo "$topics_response"
 echo
